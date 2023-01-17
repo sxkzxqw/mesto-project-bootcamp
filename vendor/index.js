b3cd44ea1a5790d4e82d3c97542429fd4cec4ca9
sxkzxqw <marsel.vagizov@bk.ru>
2023-01-17 20:29:03 +0300

    popupfeature

--- vendor/index.js
+++ vendor/index.js
@@ -3,13 +3,13 @@ const editButtonPopup = document.querySelector('.popup');
 const editButtonClosing = document.querySelector('.popup__close-button');
 const userName = document.querySelector('.profile__name');
 const userDescription = document.querySelector('.profile__description');
-const editButtonPopupInputValueName = document.getElementsByName('user-name');
-const editButtonPopupInputValueDescription = document.getElementsByName('user-description');
+const editButtonPopupInputValueName = document.querySelector('.popup__form-text_type_name');
+const editButtonPopupInputValueDescription = document.querySelector('.popup__form-text_type_description');
 // open popup
 editButton.addEventListener('click', function () {
     editButtonPopup.classList.add('popup_opened');
-    editButtonPopupInputValueName[0].setAttribute('value', userName.textContent);
-    editButtonPopupInputValueDescription[0].setAttribute('value', userDescription.textContent);
+    editButtonPopupInputValueName.value = userName.textContent;
+    editButtonPopupInputValueDescription.value = userDescription.textContent;
 });
 // close popup
 editButtonClosing.addEventListener('click', function () {
@@ -22,7 +22,19 @@ const formElement = document.querySelector('.popup__form');
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    userName.textContent = editButtonPopupInputValueName[0].getAttribute('value');
-    userDescription.textContent = editButtonPopupInputValueDescription[0].getAttribute('value');
+    userName.textContent = editButtonPopupInputValueName.value;
+    userDescription.textContent = editButtonPopupInputValueDescription.value;
 }
-formElement.addEventListener('submit', handleFormSubmit); 
+
+//function handleFormSubmitClosing(evt) {
+//    evt.preventDefault();
+//    editButtonPopup.classList.remove('popup_opened');
+//}
+
+/* formElement.addEventListener('submit', function () {
+    handleFormSubmit();
+    handleFormSubmitClosing();
+}); 
+ */
+
+formElement.addEventListener('submit', handleFormSubmit);
